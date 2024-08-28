a51476788140420eae19e7340450612f862f8a0b
Roman Podymov <podymfrombryansk@yandex.com>
2024-08-28 17:23:22 +0200

    Tests

---
+++ ParentalControlTests/ParentalControlTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParentalControlTests.swift
+//  ParentalControlTests
+//
+//  Created by Roman Podymov on 28/08/2024.
+//  Copyright © 2024 Calcium. All rights reserved.
+//
+
+//@testable import ParentalControlApp
+import XCTest
+
+class ParentalControlTests: XCTestCase {
+    func test1() {
+    
+    }
+}
